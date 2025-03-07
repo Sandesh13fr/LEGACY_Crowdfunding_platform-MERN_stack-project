ce20f223feba7790c0720021c015213dcab827f9
Sandesh13.0_jrdev <107756643+Sandesh13fr@users.noreply.github.com>
2025-03-07 17:33:43 +0530

    hmm...

--- Legacy/src/components/axiosConfig.jsx
+++ Legacy/src/components/axiosConfig.jsx
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? window.location.origin  // in production uses current domain (e.g., https://legacymd.vercel.app)
+    : 'http://localhost:5000'; // in development use localhost
+
 const api = axios.create({
-  baseURL: '/',
+  baseURL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true
+  withCredentials: true,
 });
 
 export default api;
